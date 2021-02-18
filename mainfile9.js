f6e783dc8fe985a80e814cffab46ba605d565412
Chris Giroux <chris.giroux86@gmail.com>
2021-02-18 14:13:45 -0500

    testfile7 renamed to testfile8

--- mainfile9.js
+++ mainfile9.js
@@ -1,1 +1,13 @@
 console.log('This is the main test file new777!!!!!');
+
+const IMAGE_SIZE_QUERY = `
+query ($owner: String!, $repo: String!, $sha: GitObjectID!) {
+  repository (owner: $owner, name: $repo) {
+    object (oid: $sha) {
+      ... on Blob {
+        byteSize
+      }
+    }
+  }
+}
+`;
