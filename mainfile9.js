bf0cf995f72758b2cc4553b022e01e3b572086ac
Chris Giroux <chris.giroux86@gmail.com>
2021-02-25 11:50:38 -0500

    file modified

--- mainfile9.js
+++ mainfile9.js
@@ -1,24 +1,2 @@
 
 console.log('This is the main test file now!!!!!!!!');
-
-
-const IMAGE_SIZE_QUERY = `
-query ($owner: String!, $repo: String!, $sha: GitObjectID!) {
-  repository (owner: $owner, name: $repo) {
-    object (oid: $sha) {
-      ... on Blob {
-        byteSize
-      }
-    }
-  }
-}
-`;
-
-
-
-  this.$when(() => ~this.review.core.ownerName && ~this.review.core.repoName).then(() => {
-    const core = this.review.core;
-    return this.$meta.user ?
-      this.$store.services.completions.labels.fetch(core.ownerName, core.repoName) :
-      this.$store.services.completions.labels.fetchAnonymous(core.ownerName, core.repoName);
-  });
