5c4c48cc45959657a59317614d1f47cbd5241f02
Chris Giroux <chris.giroux86@gmail.com>
2021-03-25 15:03:16 -0400

    first commit

--- lastOne.js
+++ lastOne.js
@@ -2,4 +2,8 @@
 
 console.log('HELLOOOO EVERYONE, it"s me again!!!');
 console.log('first commit!');
+<<<<<<< HEAD
 console.log('DO WE DIFF?');
+=======
+console.log('DO WE DIFF?!?');
+>>>>>>> 62c17ac... second commit
