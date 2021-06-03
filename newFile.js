b98461b6dca1170c1326f02c9f30ec92080166cd
Chris Giroux <chris.giroux86@gmail.com>
2021-06-03 14:07:22 -0400

    Multi line Commit message For testing!

--- newFile.js
+++ newFile.js
@@ -2,4 +2,3 @@ console.log('The newest of new of brand new for testing!!!');
 console.log('THIRD COMMIT!');
 console.log('FOURTH COMMIT!');
 console.log('FIFTH COMMIT!');
-console.log('6th commit');
