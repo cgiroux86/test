921bb29ee95b38848341d2fc7c750c5bb4d2a0bc
Chris Giroux <chris.giroux86@gmail.com>
2021-06-09 15:01:34 -0400

    second commit

--- added.py
+++ added.py
@@ -1,2 +1,2 @@
 print('This is an added file!!');
-print('NEW TEST!');
+print('NEW TEST!!!');
--- newFile.js
+++ newFile.js
@@ -1,1 +1,11 @@
 console.log('HI!');
+ // const filesUserIsReviewing =
+  //   _.filter(review.files, file => _.some(file.revisions, fileRev => fileRev.reviewers?.[userKey]));
+  // const lastUnkeptRevisionOfFilesUserIsReviewing = [];
+
+  // _.forEach(filesUserIsReviewing, file => {
+  //   lastUnkeptRevisionOfFilesUserIsReviewing.push(
+  //     _.findLastKey(file.revisions, fileRev => fileRev.action !== 'kept'));
+  // });
+  // const effectiveDeferralKey =
+  // _.maxBy(lastUnkeptRevisionOfFilesUserIsReviewing, key => _.parseInt(key.slice(1)));
