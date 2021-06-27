f6eaa89aed04510701a537f1c795a24ce608d5f7
Chris Giroux <chris.giroux86@gmail.com>
2021-06-27 14:42:47 -0400

    large file counts and large diffs

---
+++ generate_large_diff.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const axios = require('axios');
+
+// fs.mkdirSync('test');
+
+for (let i = 0; i < 100; i++) {
+  writeToFile(`test/large_diff_${i}`);
+}
+
+
+function writeToFile(path) {
+    const fileContents = axios.get('https://loripsum.net/api/10/long/headers')
+      .then(res => fs.writeFileSync(path, res.data))
+      .catch(err => console.log('error ->', err));
+}
