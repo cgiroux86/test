876a7d9d633ce2eec6a81551546d1f126a0ec593
Chris Giroux <chris.giroux86@gmail.com>
2021-07-26 12:03:04 -0400

    second commit

--- mainfile1.js
+++ mainfile1.js
@@ -1,2 +1,15 @@
 const REVIEWABLE_TEAM = ['github:59579733', 'github:1646896', 'github:1223704',
     'github:80698647', 'github:79605020'];
+ query ($owner: String!, $repo: String!, $commitSha: GitObjectID!) {
+repository(owner: $owner, repo: $repo, commitSha: $commitSha) {
+    object(oid: $commitSha) {
+      ... on Commit {
+        parents(first: 100) {
+          nodes {
+            id
+          }
+        }
+      }
+    }
+  }
+    }
