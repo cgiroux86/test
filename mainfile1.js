3ca33c695b200b1efecbb69bc62314c5ed32ab4e
Chris Giroux <chris.giroux86@gmail.com>
2021-08-30 19:09:24 -0400

    second commit

--- mainfile1.js
+++ mainfile1.js
@@ -10,5 +10,5 @@ function _syntheticCommitShasDifferFromRevisionCommitShas(file, revKey) {
     }
 
 const file1 = readFileSync('./file.txt', 'utf-8');
-const file2 = readFileSync('./file2.txt', 'utf-8')
+const file2 = readFileSync('./file2.txt', 'utf-8');
 
