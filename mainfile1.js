585d5ffb867c03b6d943adc82464e9fd9dd24451
Chris Giroux <chris.giroux86@gmail.com>
2021-08-31 11:59:15 -0400

    third commit

--- mainfile1.js
+++ mainfile1.js
@@ -2,4 +2,4 @@ const { readFileSync } = require("fs");
 const _ = require('lodash');
 const file1 = readFileSync('./file.txt', 'utf-8');
 const file2 = readFileSync('./file2.txt', 'utf-8');
-console.log('HELLO WORLD!!');
+console.log('HELLO WORLD!');
