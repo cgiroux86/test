2bda9f64cedfdec9ec07e98bea10f17aaaff3bbb
Chris Giroux <chris.giroux86@gmail.com>
2022-04-08 12:57:02 -0400

    large diff
    commit-id:505c46a8

--- generate_large_diffs.js
+++ generate_large_diffs.js
@@ -12,9 +12,21 @@ function execute() {
 }
 
 function writeToFile(path) {
-  return axios.get('https://loripsum.net/api/10/long/headers')
-    .then(res => fs.writeFileSync(path, res.data))
-    .catch(err => console.log('error ->', err));
+  let combined;
+  return Promise.all([
+    axios.get('https://loripsum.net/api/10/long/headers'),
+    axios.get('https://loripsum.net/api/10/long/headers'),
+    axios.get('https://loripsum.net/api/10/long/headers'),
+    axios.get('https://loripsum.net/api/10/long/headers'),
+    axios.get('https://loripsum.net/api/10/long/headers'),
+    axios.get('https://loripsum.net/api/10/long/headers')
+  ]).then(([file1, file2, file3, file4, file5, file6]) => {
+    combined = file1.data + file2.data + file3.data + file4.data + file5.data + file6.data;
+    fs.writeFileSync(`large/huge_file.html`, combined)
+  });
+  // return 
+  //   .then(res => fs.writeFileSync(path, res.data))
+  //   .catch(err => console.log('error ->', err));
 } 
 
 function removeFile(path) {
